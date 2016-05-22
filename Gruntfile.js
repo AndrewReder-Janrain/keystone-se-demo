b314a0c0599ee003a3580612d1c53cc0bed12acf
Andrew Reder <areder@janrain.com>
2016-05-22 11:20:00 -0700

    Updated keystone conf

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,50 +7,70 @@ var config= {
 module.exports = function(grunt) {
 
 	// Load grunt tasks automatically
-	require('load-grunt-tasks')(grunt);
+	// require('load-grunt-tasks')(grunt);
 
-	// Time how long tasks take. Can help when optimizing build times
-	require('time-grunt')(grunt);
+	// // Time how long tasks take. Can help when optimizing build times
+	// require('time-grunt')(grunt);
 
-	var options = {
-		config: {
-			src: './grunt/*.js'
-		},
-		pkg: grunt.file.readJSON('package.json'),
-		nodemon: {
-			serve: {
-				script: 'keystone.js',
-				options: {
-					ignore: ['node_modules/**']
-				}
-			}
-		}
-	};
+	// var options = {
+	// 	config: {
+	// 		src: './grunt/*.js'
+	// 	},
+	// 	pkg: grunt.file.readJSON('package.json'),
+	// 	nodemon: {
+	// 		serve: {
+	// 			script: 'keystone.js',
+	// 			options: {
+	// 				ignore: ['node_modules/**']
+	// 			}
+	// 		}
+	// 	}
+	// };
 
-	var configs = require('load-grunt-configs')(grunt, options);
-	
+	// var configs = require('load-grunt-configs')(grunt, options);
+
 	// Project configuration.
-	grunt.initConfig(configs);
+	grunt.initConfig(
+	{
+		sshconfig: {
+	    'digitalOcean': {
+	      host: '162.243.63.59',
+	      username: 'root',
+	      agent: 'areder',
+	      agentForward: true
+	    }
+	  },
+	  sshexec: {
+	    deploy: {
+	      command: [
+	          'cd /root/keystone-se-demo'
+	          'git pull origin master'
+	          'npm install'
+	          'touch test.txt'
+	          // 'forever stop keystone.js'
+	          // 'forever start server.js'
+	          // 'forever list'
+	        ].join(' && '),
+	        options: {
+	          config: 'digitalOcean'
+          }
+	    }
+	  }
+	});
 
 	// load jshint
-	grunt.registerTask('lint', [
-		'jshint'
+	// grunt.registerTask('lint', [
+	// 	'jshint'
+	// ]);
+	grunt.loadNpmTasks('grunt-ssh');
+	grunt.registerTask('deploy', [
+		'sshexec:deploy'
 	]);
 
-	grunt.registerTask('dev', [
-		'sass',
-		'watch'
-	]);
+	// grunt.registerTask('dev', [
+	// 	'sass',
+	// 	'watch'
+	// ]);
 
-	// default option to connect server
-	grunt.registerTask('serve', [
-		'jshint',
-		'concurrent:dev'
-	]);
-
-	grunt.registerTask('server', function () {
-		grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-		grunt.task.run(['serve:' + target]);
-	});
 
 };
